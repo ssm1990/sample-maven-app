86ed29b4ca963fba6d4a82e6e5c8c770bf0fd770
ssm1990 <55357868+ssm1990@users.noreply.github.com>
2025-12-31 17:58:21 +0530

    folder changes

---
+++ src/test/java/com/example/myapp/AppTest.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
